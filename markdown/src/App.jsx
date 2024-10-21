e182d9e58648d200a55c3d07a7c461598da4ef85
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-10-21 13:46:18 +0200

    latest commit

---
+++ markdown/src/App.jsx
@@ -0,0 +1,17 @@
+import Mydate from "./components/Mydate";
+import Citation from "./components/Citation";
+import Blague from "./components/Blague";
+
+// notre app
+function App() {
+  return (
+    <main>
+      <Mydate/>
+      <Citation/>
+      <Blague/>
+    </main>
+  );
+}
+
+
+export default App
---
+++ markdown/src/components/Blague.jsx
@@ -0,0 +1,14 @@
+import { React, useEffect, useState } from "react";
+
+function Blague() {
+
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Blague;
