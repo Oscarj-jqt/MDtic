39c7aa1995fe6a799fe77bdadd4cc0480e937adf
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-10-21 19:40:05 +0200

    tableau de bord

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Mydate from "./components/Mydate";
+import Citation from "./components/Citation";
+import Blague from "./components/Blague";
+import Event from "./components/Event";
+
+// notre app
+function App() {
+  return (
+    <main>
+      <Mydate/>
+      <Citation/>
+      <Blague/>
+      <Event/>
+    </main>
+  );
+}
+
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+
+import { createRoot } from 'react-dom/client'
+import App from './App';
+
+const racineProjet = createRoot(document.querySelector('#root'));
+
+
+racineProjet.render(<App />);
