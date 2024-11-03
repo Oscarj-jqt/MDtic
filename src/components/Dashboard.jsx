caf2e9234742451b191cd61fde2d3bf25af6fb79
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-11-03 14:38:40 +0100

    Ajout de la card Hetic

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,9 +8,14 @@ import "./Dashboard.css";
 import closeIcon from "../assets/Close.svg";
 import maximiseIcon from "../assets/Maximize.svg";
 import minimiseIcon from "../assets/Minimize.svg";
+import heticLogo from "../assets/hetic.png";
 
 // Dashboard
 function Dashboard() {
+  const openLink = (url) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <>
       <div className="top">
@@ -33,6 +38,21 @@ function Dashboard() {
           <Myrecipe />
           <Mocktail />
           <Event />
+          <li class="cards__item">
+            <div class="card">
+              <img src={heticLogo} alt="" />
+              <div class="card__content">
+                <div class="card__title">HETIC</div>
+                <p class="card__text">
+                  This website is sponsored by hetic ! A brand new school
+                  founded this year.
+                </p>
+                <button onClick={() => openLink("https://www.hetic.net/")}>
+                  See more
+                </button>
+              </div>
+            </div>
+          </li>
         </ul>
       </div>
     </>
