177d19429e4126bbf7fc07348e52999fc2b1f607
Kilian-izt <159120965+Kilian-izt@users.noreply.github.com>
2024-11-04 11:09:56 +0100

    Update Mydate.jsx

--- src/components/Mydate.jsx
+++ src/components/Mydate.jsx
@@ -2,47 +2,49 @@ import dateIcon from "../assets/calendar-0.svg";
 import timeIcon from "../assets/clock-1.svg";
 
 // Importation des modules React
+import { React, useEffect, useState } from "react";
 
-import { React, useEffect, useState, require } from "react";
-
-// Composant date
-
+// Composant pour afficher la date et l'heure actuelles
 function Mydate() {
-  // today = date.toString()
-  // Stockage de la date initiale
+  // Initialisation de la date actuelle
   const date = new Date();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // Mois actuel (+1 car Janvier = 0)
+  const day = date.getDate(); // Jour actuel
+  const year = date.getFullYear(); // Année actuelle
 
+  // Récupération de l'heure actuelle (heures et minutes)
   const time = date.getHours() + ":" + date.getMinutes();
-  // Stockage du temps à partir de "date"
-  // Gestion de l'état (useState) de la date pour la màj
+
+  // Gestion de l'état pour stocker l'heure actuelle
   const [currenttime, setTime] = useState(time);
 
-  // Système de màj toute les minutes (useEffect)
+  // Utilisation de useEffect pour mettre à jour l'heure toutes les minutes
   useEffect(() => {
+    // Création de l'intervalle pour actualiser l'heure toutes les 60 secondes
     const objetInterval = setInterval(() => {
-      // On relance la fonction Date()
+      // Récupération de la nouvelle heure
       const upDate = new Date();
       const newTime = upDate.getHours() + ":" + upDate.getMinutes();
-      setTime(newTime);
-    }, 60000);
+      setTime(newTime); // Mise à jour de l'état avec la nouvelle heure
+    }, 60000); // 60000 ms = 1 minute
 
+    // Nettoyage de l'intervalle pour éviter les fuites de mémoire
     return () => {
       clearInterval(objetInterval);
     };
-  }, []);
+  }, []); // Le tableau vide indique que cet effet s'exécute une seule fois au montage
 
   return (
     <div className="callendar">
+      {/* Affichage de la date actuelle */}
       <div className="date">
-        <img src={dateIcon} alt="" />
-        Today is {month} / {day} / {year}
+        <img src={dateIcon} alt="" /> {/* Icône de calendrier */}
+        Today is {month} / {day} / {year} {/* Affichage de la date */}
       </div>
+      {/* Affichage de l'heure actuelle */}
       <div className="time">
-        <img src={timeIcon} />
-        Time is {time}
+        <img src={timeIcon} alt="" /> {/* Icône de l'horloge */}
+        Time is {currenttime} {/* Affichage de l'heure */}
       </div>
     </div>
   );
